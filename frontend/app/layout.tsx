25e81004005a0b7418461249634d29c2f4dfb846
ronittalreja <talrejaronit13@gmail.com>
2025-10-23 17:46:26 +0530

    favicon fixx

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -17,10 +17,6 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <head>
-        <link rel="icon" href="/favicon.svg" type="image/svg+xml" />
-        <link rel="alternate icon" href="/favicon.ico" />
-      </head>
       <body className={`font-sans ${GeistSans.variable} ${GeistMono.variable}`}>
         {children}
         <Analytics />
